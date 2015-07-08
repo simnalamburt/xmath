e7d88a93250fc70637ae88a06e304c71ad473380
Seulgi Kim <dev@seulgi.kim>
2015-07-08 20:43:28 +0900

    Add a test case for Vector2.transform()

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,25 @@ impl Vector for Vector2 {
     }
 }
 
+#[test]
+fn transform_vector2() {
+    let matrix = Matrix {
+        m: [
+            [ 2.0, 3.0, 5.0, 7.0 ],
+            [ 11.0, 13.0, 17.0, 19.0 ],
+            [ 23.0, 29.0, 31.0, 37.0 ],
+            [ 41.0, 43.0, 47.0, 53.0 ],
+        ]
+    };
+
+    let origin = (1.0, 1.0);
+
+    let transformed = origin.transform(&matrix);
+
+    assert_eq!(transformed.0, 13.0);
+    assert_eq!(transformed.1, 16.0);
+}
+
 #[derive(PartialEq, Clone, Debug)]
 pub struct Matrix { m: [[f32; 4]; 4] }
 
